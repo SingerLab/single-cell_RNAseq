601a95590b822cfc8e8c5c1ea02933bfe34647d8
Rodrigo Gularte Merida <gularter@mskcc.org>
2025-09-30 10:30:01 -0400

    working changes for repo organization

--- src/01_cellranger.sh
+++ src/01_cellranger.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 #BSUB -n 12 -R "rusage[mem=7]" -W 64:00 -R "span[hosts=1]"
-MEM_PER_JOB=$(echo $(printf "%d\n" ${LSB_CG_MEMLIMIT} )/1024^3 | bc ) ## $( echo $LSB_SUB_RES_REQ | sed -e 's/.*=//' -e 's/]//' )
+MEM_PER_JOB=$(echo $(printf "%d\n" ${LSB_CG_MEMLIMIT} )/1024^3 | bc )
 MAX_MEM=$(( ${MEM_PER_JOB} * $LSB_MAX_NUM_PROCESSORS - 1 ))
 
 source /work/singer/opt/miniconda3/bin/activate single-cell-rnaseq
 
-#<usage>
-[[ $# -eq 5 ]] || {
-echo "Usage:"
-echo "This script expects the required arcuments to run cellranger"
-echo " in order:  output directory, path/to/fastq/ \"sample_name\", \"project name\", and transcriptome directory"
-echo ""
-echo ""
-echo 'bsub -n 8 -R "rusage[mem=7]" -W 48:00 -R "span[hosts=1]" "./src/01_cellranger.sh ML1105_01 seqdata/Sample_ML1105_IGO_09649_1/ ML1105_IGO_09649_1  "ML1105" "cellranger" > log/ML1105_01/cellranger.log 2>&1"'
-echo ""
-exit 1; }
-#</usage>
-set -e -x -o pipefail -u
+set -euox pipefail
 
-OUTDIR=$1            ## ML1105_01
-FASTQ_DIR=$2         ## seqdata/Sample_ML1105_IGO_09649_1/
-SAMPLE=$3            ## ML1105_IGO_09649_1
-PROJECT_NAME=$4      ## "ML1105"
-TRANSCRIPTOME=$5     ## cellranger directory name
+usage() {
+    echo "Usage: $0 -g <transcriptome_ref> -p <project_name> -o <output_dir> -s <sample_name> -i <fastq_path>"
+    exit 1
+}
+
+while getopts ":g:p:o:s:i:" opt; do
+    case ${opt} in
+	g ) TRANSCRIPTOME=$OPTARG ;;  ## hg38
+	p ) PROJECT_NAME=$OPTARG ;;   ## ML1105
+	o ) OUTDIR=$OPTARG ;;         ## ML1105_01 
+	s ) SAMPLE=$OPTARG ;;         ## ML1105_IGO_09649_1
+	i ) FASTQ_DIR=$OPTARG ;;      ## seqdata/Sample_ML1105_IGO_09649_1/
+	\? ) echo "Invalid option: -$OPTARG" >&2; usage ;;
+	: ) echo "Option -$OPTARG requires an argument." >&2; usage ;;
+    esac
+done
+
+if [ -z "${TRANSCRIPTOME:-}" ] || [ -z "${PROJECT_NAME:-}" ] || [ -z "${OUTDIR:-}" ] || [ -z "${SAMPLE:-}" ] || [ -z "${FASTQ_DIR:-}" ]; then
+    usage
+fi
+
+echo "[INFO] Running Cell Ranger count for gene expression..."
+
 
 ## cell ranger version
-CELLRANGER=/work/singer/opt/src/cellranger-7.1.0/cellranger
+##use v9.0.1 for TIL & MEK
+CELLRANGER=/work/singer/opt/src/cellranger-9.0.1/bin/cellranger 
+## /work/singer/opt/src/cellranger-7.1.0/cellranger    
 #% ~/src/cellranger-3.0.2/cellranger
 #% /work/singer/opt/src/cellranger-6.0.1/cellranger
 ## differences between 3.0.2 and 6.0.1 are 10-30 less cells in 6.0.1; As of 23.Apr.2021 we keep 3.0.2
 ## will identify if counts are more precise at a later point
-$CELLRANGER count --id=${OUTDIR} \
- 	    --fastqs=${FASTQ_DIR} \
+$CELLRANGER count --create-bam true \
+	    --id=${OUTDIR} \
+	    --fastqs=${FASTQ_DIR} \
 	    --sample=${SAMPLE} \
 	    --transcriptome=${TRANSCRIPTOME} \
 	    --project=${PROJECT_NAME} \
 	    --localcores=${LSB_MAX_NUM_PROCESSORS} \
 	    --localmem=${MAX_MEM} \
 	    --nosecondary
+
+echo "[INFO] Cell Ranger count completed for ${SAMPLE}."
 
 [[ ! -d ${OUTDIR} ]] || rm -rf ${OUTDIR}/SC_RNA_COUNTER_CS/
 [[ ! -d ${OUTDIR} ]] || rm -rf ${OUTDIR}/analysis/
@@ -47,3 +59,5 @@ VELOCYTO_SCRIPT=./src/02_velocyto.sh
 
 bsub -n 12 -R "rusage[mem=5]" -W 64:00 -R "span[hosts=1]" "$VELOCYTO_SCRIPT ${TRANSCRIPTOME} ${OUTDIR}"
 
+
+##__EOF__
--- src/01_vdj.sh
+++ src/01_vdj.sh
@@ -1,53 +1,64 @@
 #!/bin/bash
 #BSUB -n 12 -R "rusage[mem=7]" -W 64:00 -R "span[hosts=1]"
-MEM_PER_JOB=$(echo $(printf "%d\n" ${LSB_CG_MEMLIMIT} )/1024^3 | bc ) ## $( echo $LSB_SUB_RES_REQ | sed -e 's/.*=//' -e 's/]//' )
+MEM_PER_JOB=$(echo $(printf "%d\n" ${LSB_CG_MEMLIMIT} )/1024^3 | bc )
 MAX_MEM=$(( ${MEM_PER_JOB} * $LSB_MAX_NUM_PROCESSORS - 1 ))
 
 ## source /work/singer/opt/miniconda3/bin/activate single-cell-rnaseq
 
-#<usage>
-[[ $# -eq 5 ]] || {
-echo "Usage:"
-echo "This script expects the required arcuments to run cellranger"
-echo " in order:  output directory, path/to/fastq/ \"sample_name\", \"project name\", and transcriptome directory"
-echo ""
-echo ""
-echo 'bsub -n 8 -R "rusage[mem=7]" -W 48:00 -R "span[hosts=1]" "./src/01_cellranger.sh ML1105_01 seqdata/Sample_ML1105_IGO_09649_1/ ML1105_IGO_09649_1  "ML1105" "cellranger" > log/ML1105_01/cellranger.log 2>&1"'
-echo ""
-exit 1; }
-#</usage>
-set -e -x -o pipefail -u
+#!/bin/bash
+set -euo pipefail
 
-OUTDIR=$1            ## ML1105_01
-FASTQ_DIR=$2         ## seqdata/Sample_ML1105_IGO_09649_1/
-SAMPLE=$3            ## ML1105_IGO_09649_1
-PROJECT_NAME=$4      ## "ML1105"
-REFERENCE=$5     ## cellranger directory name
+usage() {
+    echo "Usage: $0 -g <vdj_reference> -p <project_name> -o <output_dir> -s <sample_name> -i <fastq_path>"
+    exit 1
+}
+
+while getopts ":g:p:o:s:i:" opt; do
+    case ${opt} in
+	g ) REFERENCE=$OPTARG ;;      ## cellranger VDJ directory name 
+	p ) PROJECT_NAME=$OPTARG ;;   ## ML1105
+	o ) OUTDIR=$OPTARG ;;         ## ML1105_01
+	s ) SAMPLE=$OPTARG ;;         ## ML1105_IGO_09649_1
+	i ) FASTQ_DIR=$OPTARG ;;      ## seqdata/Sample_ML1105_IGO_09649_1/
+	\? ) echo "Invalid option: -$OPTARG" >&2; usage ;;
+	: ) echo "Option -$OPTARG requires an argument." >&2; usage ;;
+    esac
+done
+
+if [ -z "${REFERENCE:-}" ] || [ -z "${PROJECT_NAME:-}" ] || [ -z "${OUTDIR:-}" ] || [ -z "${SAMPLE:-}" ] || [ -z "${FASTQ_DIR:-}" ]; then
+    usage
+fi
+
+echo "[INFO] Running Cell Ranger VDJ for immune profiling..."
 
 ## cell ranger version
 CELLRANGER=/work/singer/opt/src/cellranger-7.1.0/cellranger
 
+## check if VDJ reference or convert
+if [[ "$REFERENCE" != *_vdj ]]; then
+    REFERENCE="${REFERENCE}_vdj"
+fi
+
 ## reference analysis
 $CELLRANGER vdj --id=${OUTDIR} \
 	    --reference=${REFERENCE} \
- 	    --fastqs=${FASTQ_DIR} \
+	    --fastqs=${FASTQ_DIR} \
 	    --sample=${SAMPLE} \
 	    --project=${PROJECT_NAME} \
 	    --localcores=${LSB_MAX_NUM_PROCESSORS} \
-	    --localmem=${MAX_MEM} 
+	    --localmem=${MAX_MEM}
 
 ## denovo analysis
 $CELLRANGER vdj --id=${OUTDIR}_DENOVO \
 	    --reference=${REFERENCE} \
- 	    --fastqs=${FASTQ_DIR} \
+	    --fastqs=${FASTQ_DIR} \
 	    --sample=${SAMPLE} \
 	    --project=${PROJECT_NAME} \
 	    --denovo \
 	    --localcores=${LSB_MAX_NUM_PROCESSORS} \
-	    --localmem=${MAX_MEM} 
+	    --localmem=${MAX_MEM}
 
+echo "[INFO] Cell Ranger VDJ completed for ${SAMPLE}."
 
-## [[ ! -d ${OUTDIR} ]] || rm -rf ${OUTDIR}/SC_RNA_COUNTER_CS/
-## [[ ! -d ${OUTDIR} ]] || rm -rf ${OUTDIR}/analysis/
-
+## __EOF__
 
